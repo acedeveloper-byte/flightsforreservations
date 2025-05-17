2fe9a4bf3d784adad1d51701475bf6024f44a885
Mayank-Anonymously <craftaar0@gmail.com>
2025-05-17 17:39:04 +0530

    added changes

--- app/layout.js
+++ app/layout.js
@@ -13,12 +13,26 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Flights For Reservations | Find Cheap Flights",
-  description: "Book your next flight with flightsforreservations.com, and score big savings. Find exclusive deals and offers now!",
+  description:
+    "Book your next flight with flightsforreservations.com, and score big savings. Find exclusive deals and offers now!",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16951756852"
+        ></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+                    
+            gtag('config', 'AW-16951756852');`}
+        </script>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
